681a486c9b20e63eded4082fac3950b93736f7c2
Francisco Vargas <falisson.sv@gmail.com>
2020-07-25 15:11:31 -0300

    First Commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,44 @@
+import { Router } from 'express';
+import { parseISO, parse } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (request, response) => {
+  try {
+		const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+		const appointments = await appointmentsRepository.find();
+
+		return response.json(appointments);
+		
+  } catch (err) {
+		return response.status(400). json({ error: err.message });
+  }
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+  try {
+		const { provider, date } = request.body;
+
+		const parsedDate = parseISO(date);
+
+		const createAppointment = new CreateAppointmentService();
+
+		const appointment = await createAppointment.execute({
+			date: parsedDate,
+			provider,
+		});
+
+		return response.json(appointment);
+
+
+  } catch (err) {
+		return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
