bb217297fe9ab14663b41f91cb2c0de5be5f510c
Francisco Vargas <falisson.sv@gmail.com>
2020-07-25 17:23:31 -0300

    Create authentication middleware

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+	jwt: {
+		secret: 'bea66d0afad5614625f9f41daae3cb5c',
+		expiresIn: '1d'
+	}
+}
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,7 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-
+import authConfig from '../config/auth'
 
 import User from '../models/User';
 
@@ -31,9 +31,11 @@ class AuthenticateUserService {
 			throw new Error('Incorrect email/password combination.')
 
 
-		const token = sign({}, 'bea66d0afad5614625f9f41daae3cb5c', {
+		const { secret, expiresIn } = authConfig.jwt
+
+		const token = sign({}, secret, {
 			subject: user.id,
-			expiresIn: '1d'
+			expiresIn
 		});
 
 		return {
